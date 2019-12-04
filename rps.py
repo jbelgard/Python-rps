14152cd5873dea489da57a0c64ab08120e85e813
Jason Belgard <belgard.jason@gmail.com>
2019-12-04 11:09:21 -0600

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,17 @@
+
+# Create a RPL loop to process commands
+while True: # Loop
+    # Read
+    cmd = input("-> ")
+
+    # RPL should accept 'r', 'p', 's' commands
+    # 'q' to quit
+    # Eval
+    if cmd == "r":
+        # Do something
+    elif cmd == "p"
+
+    # When a player types r/p/s, CPU will pick a random choice
+
+    # Print out the score and loop
+
